28eee1ccba4feca5f56228b79d433dbaf5eb329d
Olivier Van hamme <nicefiction@gmail.com>
2022-04-01 15:28:15 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Olivier Van hamme on 01/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
