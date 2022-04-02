0846221ee3ffc3466122970d36813b32afe6abcd
Olivier Van hamme <nicefiction@gmail.com>
2022-04-02 20:11:00 +0200

    Completes Day 17.2: Creating pickers in a form.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -6,7 +6,17 @@ struct ContentView: View {
     
     // MARK: - PROPERTY WRAPPERS
     
-    @State private var amount: Double = 0.00
+    @State private var checkAmount: Double = 0.00
+    @State private var selectedNumberOfParticipants: Int = 0
+    @State private var selectedTipPercentage: Int = 15
+    
+    
+    
+    // MARK: - Properties
+    
+    var tipPercentages: Array<Int> = [
+        5, 10, 15, 20, 25
+    ]
     
     
     
@@ -14,23 +24,41 @@ struct ContentView: View {
     
     var body: some View {
         
-        Form {
-            Section {
-                TextField("Enter your amount:",
-                          value: $amount,
-                          format: .currency(code: Locale.current.currencyCode ?? "USD"))
-                .padding()
-                .keyboardType(UIKeyboardType.decimalPad)
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Enter your amount:",
+                              value: $checkAmount,
+                              format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    .keyboardType(UIKeyboardType.decimalPad)
+                    Picker("Divided by",
+                           selection: $selectedNumberOfParticipants) {
+                        ForEach(2..<10) {
+                            Text("\($0)")
+                        }
+                    }
+                }
+                Section {
+                    Text(checkAmount,
+                         format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                }
+    //            Section {
+    //                Text(Decimal.FormatStyle.Currency.FormatInput(amount),
+    //                     format: Decimal.FormatStyle.Currency.currency(code: Locale.current.currencyCode ?? "EUR"))
+    //            }
             }
-            Section {
-                Text(amount,
-                     format: .currency(code: Locale.current.currencyCode ?? "USD"))
-            }
+            .navigationTitle("We Split")
         }
     }
 }
 
-
+/*
+ Picker("Number of people", selection: $numberOfPeople) {
+         ForEach(2 ..< 100) {
+             Text("\($0) people")
+         }
+     }
+ */
 
 
 
